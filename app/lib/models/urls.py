8ce4c86567f9638797c0e5dd191a6bd7fde23884
farioso_fernando <farioso.f@gmail.com>
2023-01-12 08:52:52 +0200

    add: novas urls adicionadas.

--- app/lib/models/urls.py
+++ app/lib/models/urls.py
@@ -1,2 +1,12 @@
 URL_BAZARA = "https://bazara.co.mz"
+
 URL_BEBIDAS = URL_BAZARA + '/bebidas.html'
+URL_LIVROS = URL_BAZARA + '/livraria'
+URL_ALIMENTOS = URL_BAZARA + '/mercearia.html'
+URL_ELETRONICOS = URL_BAZARA + '/eletronicos-acessorios'
+URL_CASA = URL_BAZARA +  '/utilidades-domesticas'
+URL_PAPELARIA =  URL_BAZARA + '/papelaria-escritorio.html'
+URL_SAUDE = URL_BAZARA + '/saude-beleza.html'
+URL_BEBE = URL_BAZARA + '/bebe-e-crianca.html'
+URL_VESTUARIO = URL_BAZARA + '/vestuario-calcado'
+URL_MUSIC = URL_BAZARA + '/musica'
