5d92af8e6c1acbdda3df9b2d8a0bf163a9b216fe
farioso_fernando <farioso.f@gmail.com>
2023-01-15 23:43:55 +0200

    recheio model added.

---
+++ app/models/recheio_co_mz.py
@@ -0,0 +1,54 @@
+mercearia = 'https://recheio.co.mz/products/Default?cat=groceries24A5725'
+lacticinios = 'https://recheio.co.mz/products/Default?cat=dairy-products'
+carne_peixe = 'https://recheio.co.mz/products/Default?cat=meat--seafood'
+bebidas_frias = 'https://recheio.co.mz/products/Default?cat=cold-beverages'
+bebidas_quentes = 'https://recheio.co.mz/products/Default?cat=hot-beverages'
+cuidados_pessoais = 'https://recheio.co.mz/products/Default?cat=personal-care'
+vegetais = 'https://recheio.co.mz/products/Default?cat=vegetables'
+bebe = 'https://recheio.co.mz/products/Default?cat=baby'
+produtos_beleza = 'https://recheio.co.mz/products/Default?cat=beauty-products'
+cuidados_domesticos = 'https://recheio.co.mz/products/Default?cat=home-care--cleaning'
+papelaria = 'https://recheio.co.mz/products/Default?cat=stationaries'
+loica_decoracao = 'https://recheio.co.mz/products/Default?cat=kitchen--dining'
+animais = 'https://recheio.co.mz/products/Default?cat=pets'
+lanches = 'https://recheio.co.mz/products/Default?cat=snacks--chocolate'
+loja_asiatica = 'https://recheio.co.mz/products/Default?cat=asian-store'
+eletronicos = 'https://recheio.co.mz/products/Default?cat=electric-appliances'
+
+ALL_URLS = [
+mercearia,
+lacticinios,
+carne_peixe,
+bebidas_frias,
+bebidas_quentes,
+cuidados_pessoais,
+vegetais,
+bebe,
+produtos_beleza,
+cuidados_domesticos,
+papelaria,
+loica_decoracao,
+animais,
+lanches,
+loja_asiatica,
+eletronicos,
+]
+
+NAMES = [
+'mercearia',
+'lacticinios',
+'carne_peixe',
+'bebidas_frias',
+'bebidas_quentes',
+'cuidados_pessoais',
+'vegetais',
+'bebe',
+'produtos_beleza',
+'cuidados_domesticos',
+'papelaria',
+'loica_decoracao',
+'animais',
+'lanches',
+'loja_asiatica',
+'eletronicos',
+]
---
+++ recheio_run.py
@@ -0,0 +1,60 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from cockroach import developing_cockroach as developer
+import time
+
+from app.blocs.excel import BlocExcel
+from app.models.recheio_co_mz import ALL_URLS, NAMES
+from walls.recheio_co_mz.scrappy import next_page, scraping
+
+
+print('Calling the browser. Wait a moment.')
+
+firefox = webdriver.Firefox()
+
+def scroll():
+    total_page_height = firefox.execute_script("return document.body.scrollHeight")
+    browser_window_height = firefox.get_window_size(windowHandle='current')['height']
+    current_position = firefox.execute_script('return window.pageYOffset')
+    
+    while total_page_height - current_position > browser_window_height:
+        firefox.execute_script(f"window.scrollTo({current_position}, {browser_window_height + current_position});")
+        current_position = firefox.execute_script('return window.pageYOffset')
+        time.sleep(5)  # It is necessary here to give it some time to load the content
+
+# Começando a varredura.
+def start_scan(document:BlocExcel):
+    
+    print('Aguarde, rolando pagina.')
+    scroll()
+    print('Rolagem terminada -> Começando a extração.')
+
+    row = firefox.find_element(By.CLASS_NAME, "ps-shopping-product")    
+    row_ProductList = row.find_element(By.TAG_NAME, "div")
+
+    list_item = row_ProductList.find_elements(By.XPATH, "//div[@class=' col-lx-1 col-lg-4 col-md-4 col-sm-6 col-6']")
+
+    scraping(document, list_item)
+
+def setup():
+
+    for URL, NAME in zip(ALL_URLS, NAMES):
+        
+        # Inicializando o documento excel.
+        _document = BlocExcel(fileName='recheio_category_'+NAME, columnNames=['PRODUCT_NAME', 'PRODUCT_LINK', 'PRODUCT_PRICE'], sheetName='Sheet')
+        
+        firefox.get(URL) # Pegando a categoria
+
+        while True:
+            print('Starting scan in 2 second...')
+            time.sleep(10)
+            start_scan(_document) # primeiro pega os dados da pagina.
+            if next_page(browser=firefox): # depois vefica se ainda há paginas.
+                continue # continua caso hajam paginas.
+            else:
+                _document.generate_and_save_excel()
+                break # caso contrario
+
+setup()
+
+developer.log('✔ Program finished. Found errors. 0.', name= 'Scraping')
