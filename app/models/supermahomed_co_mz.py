62facc30d87b3e6d8ddcacde7ac79db60c4afb6b
farioso_fernando <farioso.f@gmail.com>
2023-01-16 17:16:23 +0200

    feature: supermahomed como modulo adicionado.

---
+++ app/models/supermahomed_co_mz.py
@@ -0,0 +1,22 @@
+SUPERMAHOMED_URL = 'https://www.supermahomed.co.mz'
+
+mercearia = SUPERMAHOMED_URL + '/11-mercearia'
+bebidas = SUPERMAHOMED_URL + '/28-bebidas'
+higiene = SUPERMAHOMED_URL + '/30-higiene-pessoal-'
+casa = SUPERMAHOMED_URL + '/31-casa-limpeza'
+bebe = SUPERMAHOMED_URL + '/29-bebe'
+
+ALL_URLS = [
+    # mercearia,
+    # bebidas,
+    # higiene,
+    # casa,
+    bebe
+ ]
+NAMES = [
+    # 'mercearia',
+    # 'bebidas',
+    # 'higiene',
+    # 'casa',
+    'bebe'
+        ]
