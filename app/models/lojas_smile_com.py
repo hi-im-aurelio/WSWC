2d55fbc9926a2c5a58892b43ada8b8f004fb5cfb
farioso_fernando <farioso.f@gmail.com>
2023-01-16 03:57:01 +0200

    feature: lojas smaile added.

---
+++ app/models/lojas_smile_com.py
@@ -0,0 +1,294 @@
+
+SMILE_URL = 'https://lojassmile.com'
+
+URL_1 = SMILE_URL + "/categoria-produto/bengaleiros/"
+URL_2 = SMILE_URL + "/categoria-produto/capsulas-cafe/"
+URL_3 = SMILE_URL + "/categoria-produto/casa-de-banho/"
+URL_4 = SMILE_URL + "/categoria-produto/cozinhas/"
+URL_5 = SMILE_URL + "/categoria-produto/cozinhas/iluminacao-cozinhas/"
+URL_6 = SMILE_URL + "/categoria-produto/cozinhas/menage/"
+URL_7 = SMILE_URL + "/categoria-produto/cozinhas/mesas_cad/"
+URL_8 = SMILE_URL + "/categoria-produto/cozinhas/torneiras/"
+URL_9 = SMILE_URL + "/categoria-produto/decoracao/"
+URL_10 = SMILE_URL + "/categoria-produto/decoracao/casticais/"
+URL_11 = SMILE_URL + "/categoria-produto/decoracao/jarras/"
+URL_12 = SMILE_URL + "/categoria-produto/decoracao/molduras-de-parede/"
+URL_13 = SMILE_URL + "/categoria-produto/decoracao/relogio/"
+URL_14 = SMILE_URL + "/categoria-produto/decoracao/telas-imagens/"
+URL_15 = SMILE_URL + "/categoria-produto/decoracao/texteis/"
+URL_16 = SMILE_URL + "/categoria-produto/decoracao/texteis/almofadas/"
+URL_17 = SMILE_URL + "/categoria-produto/decoracao/texteis/roupa-de-cama/"
+URL_18 = SMILE_URL + "/categoria-produto/decoracao/texteis/tapetes/"
+URL_19 = SMILE_URL + "/categoria-produto/electrodomesticos/"
+URL_20 = SMILE_URL + "/categoria-produto/electrodomesticos/ar_condicionado/"
+URL_21 = SMILE_URL + "/categoria-produto/electrodomesticos/asp/"
+URL_22 = SMILE_URL + "/categoria-produto/electrodomesticos/encastraveis/"
+URL_23 = SMILE_URL + "/categoria-produto/electrodomesticos/encastraveis/chamines/"
+URL_24 = SMILE_URL + "/categoria-produto/electrodomesticos/encastraveis/fornos/"
+URL_25 = SMILE_URL + "/categoria-produto/electrodomesticos/encastraveis/maquina-de-lavar-loica/"
+URL_26 = SMILE_URL + "/categoria-produto/electrodomesticos/encastraveis/maquina-de-lavar-roupa/"
+URL_27 = SMILE_URL + "/categoria-produto/electrodomesticos/encastraveis/micro-ondas-encastraveis/"
+URL_28 = SMILE_URL + "/categoria-produto/electrodomesticos/encastraveis/placas/"
+URL_29 = SMILE_URL + "/categoria-produto/electrodomesticos/esquentadores/"
+URL_30 = SMILE_URL + "/categoria-produto/electrodomesticos/frig_gel_congel/"
+URL_31 = SMILE_URL + "/categoria-produto/electrodomesticos/maq_caf/"
+URL_32 = SMILE_URL + "/categoria-produto/electrodomesticos/maq_lave_sec/"
+URL_33 = SMILE_URL + "/categoria-produto/electrodomesticos/maq_lav_loica/"
+URL_34 = SMILE_URL + "/categoria-produto/electrodomesticos/pequenos_electrodomesticos/"
+URL_35 = SMILE_URL + "/categoria-produto/electrodomesticos/pequenos_electrodomesticos/batedeiras/"
+URL_36 = SMILE_URL + "/categoria-produto/electrodomesticos/pequenos_electrodomesticos/cuidado-pessoal/"
+URL_37 = SMILE_URL + "/categoria-produto/electrodomesticos/pequenos_electrodomesticos/ferros-a-vapor/"
+URL_38 = SMILE_URL + "/categoria-produto/electrodomesticos/pequenos_electrodomesticos/grelhadores/"
+URL_39 = SMILE_URL + "/categoria-produto/electrodomesticos/pequenos_electrodomesticos/pequeno-almoco/"
+URL_40 = SMILE_URL + "/categoria-produto/electrodomesticos/pequenos_electrodomesticos/robots-de-cozinha/"
+URL_41 = SMILE_URL + "/categoria-produto/electrodomesticos/pequenos_electrodomesticos/varinha-magica/"
+URL_42 = SMILE_URL + "/categoria-produto/electrodomesticos/termo-acumuladores/"
+URL_43 = SMILE_URL + "/categoria-produto/electrodomesticos/term_acomul/"
+URL_44 = SMILE_URL + "/categoria-produto/electrodomesticos/uten_lar/"
+URL_45 = SMILE_URL + "/categoria-produto/escritorio/"
+URL_46 = SMILE_URL + "/categoria-produto/escritorio/acessorios-de-escritorio/"
+URL_47 = SMILE_URL + "/categoria-produto/escritorio/arruma_escritorio/"
+URL_48 = SMILE_URL + "/categoria-produto/escritorio/cadeiras_escritorio/"
+URL_49 = SMILE_URL + "/categoria-produto/escritorio/mesa_secretaria/"
+URL_50 = SMILE_URL + "/categoria-produto/iluminacao/"
+URL_51 = SMILE_URL + "/categoria-produto/marcas/"
+URL_52 = SMILE_URL + "/categoria-produto/marcas/balay/"
+URL_53 = SMILE_URL + "/categoria-produto/marcas/bosch/"
+URL_54 = SMILE_URL + "/categoria-produto/marcas/candy/"
+URL_55 = SMILE_URL + "/categoria-produto/marcas/cata/"
+URL_56 = SMILE_URL + "/categoria-produto/marcas/frasa/"
+URL_57 = SMILE_URL + "/categoria-produto/marcas/hoover/"
+URL_58 = SMILE_URL + "/categoria-produto/marcas/ikea/"
+URL_59 = SMILE_URL + "/categoria-produto/marcas/lg/"
+URL_60 = SMILE_URL + "/categoria-produto/marcas/meireles/"
+URL_61 = SMILE_URL + "/categoria-produto/marcas/moulinex/"
+URL_62 = SMILE_URL + "/categoria-produto/marcas/nespresso/"
+URL_63 = SMILE_URL + "/categoria-produto/marcas/rodi/"
+URL_64 = SMILE_URL + "/categoria-produto/marcas/rowenta/"
+URL_65 = SMILE_URL + "/categoria-produto/marcas/samsung/"
+URL_66 = SMILE_URL + "/categoria-produto/marcas/siemens/"
+URL_67 = SMILE_URL + "/categoria-produto/marcas/smile/"
+URL_68 = SMILE_URL + "/categoria-produto/marcas/teka/"
+URL_69 = SMILE_URL + "/categoria-produto/marcas/ufesa/"
+URL_70 = SMILE_URL + "/categoria-produto/marcas/whirlpool/"
+URL_71 = SMILE_URL + "/categoria-produto/marcas/zanussi/"
+URL_72 = SMILE_URL + "/categoria-produto/micro-ondas/"
+URL_73 = SMILE_URL + "/categoria-produto/moveis/"
+URL_74 = SMILE_URL + "/categoria-produto/moveis/espelhos/"
+URL_75 = SMILE_URL + "/categoria-produto/moveis/molduras-e-quadros/"
+URL_76 = SMILE_URL + "/categoria-produto/moveis/quarto/"
+URL_77 = SMILE_URL + "/categoria-produto/moveis/quarto/camas/"
+URL_78 = SMILE_URL + "/categoria-produto/moveis/quarto/colchoes/"
+URL_79 = SMILE_URL + "/categoria-produto/moveis/quarto/comodas/"
+URL_80 = SMILE_URL + "/categoria-produto/moveis/quarto/iluminacao-quarto/"
+URL_81 = SMILE_URL + "/categoria-produto/moveis/quarto/mesas_cabec/"
+URL_82 = SMILE_URL + "/categoria-produto/moveis/quarto/roupeiros/"
+URL_83 = SMILE_URL + "/categoria-produto/moveis/quarto/toucador/"
+URL_84 = SMILE_URL + "/categoria-produto/moveis/sala/"
+URL_85 = SMILE_URL + "/categoria-produto/moveis/sala/arrumacao-de-sala/"
+URL_86 = SMILE_URL + "/categoria-produto/moveis/sala/cadeiras/"
+URL_87 = SMILE_URL + "/categoria-produto/moveis/sala/mesas/"
+URL_88 = SMILE_URL + "/categoria-produto/moveis/sala/mesas-de-centro-apoio/"
+URL_89 = SMILE_URL + "/categoria-produto/moveis/sala/moveis-tv/"
+URL_90 = SMILE_URL + "/categoria-produto/moveis/sala/sofas-poltronas/"
+URL_91 = SMILE_URL + "/categoria-produto/moveis/sapateiras/"
+URL_92 = SMILE_URL + "/categoria-produto/moveis/vitrines/"
+URL_93 = SMILE_URL + "/categoria-produto/moveis-2/"
+URL_94 = SMILE_URL + "/categoria-produto/sofas/"
+URL_95 = SMILE_URL + "/categoria-produto/tabuas-de-engomar/"
+
+ALL_URLS = [
+    URL_1,
+URL_2,
+URL_3,
+URL_4,
+URL_5,
+URL_6,
+URL_7,
+URL_8,
+URL_9,
+URL_10,
+URL_11,
+URL_12,
+URL_13,
+URL_14,
+URL_15,
+URL_16,
+URL_17,
+URL_18,
+URL_19,
+URL_20,
+URL_21,
+URL_22,
+URL_23,
+URL_24,
+URL_25,
+URL_26,
+URL_27,
+URL_28,
+URL_29,
+URL_30,
+URL_31,
+URL_32,
+URL_33,
+URL_34,
+URL_35,
+URL_36,
+URL_37,
+URL_38,
+URL_39,
+URL_40,
+URL_41,
+URL_42,
+URL_43,
+URL_44,
+URL_45,
+URL_46,
+URL_47,
+URL_48,
+URL_49,
+URL_50,
+URL_51,
+URL_52,
+URL_53,
+URL_54,
+URL_55,
+URL_56,
+URL_57,
+URL_58,
+URL_59,
+URL_60,
+URL_61,
+URL_62,
+URL_63,
+URL_64,
+URL_65,
+URL_66,
+URL_67,
+URL_68,
+URL_69,
+URL_70,
+URL_71,
+URL_72,
+URL_73,
+URL_74,
+URL_75,
+URL_76,
+URL_77,
+URL_78,
+URL_79,
+URL_80,
+URL_81,
+URL_82,
+URL_83,
+URL_84,
+URL_85,
+URL_86,
+URL_87,
+URL_88,
+URL_89,
+URL_90,
+URL_91,
+URL_92,
+URL_93,
+URL_94,
+URL_95,
+]
+
+NAMES = [
+'URL_1',
+'URL_2',
+'URL_3',
+'URL_4',
+'URL_5',
+'URL_6',
+'URL_7',
+'URL_8',
+'URL_9',
+'URL_10',
+'URL_11',
+'URL_12',
+'URL_13',
+'URL_14',
+'URL_15',
+'URL_16',
+'URL_17',
+'URL_18',
+'URL_19',
+'URL_20',
+'URL_21',
+'URL_22',
+'URL_23',
+'URL_24',
+'URL_25',
+'URL_26',
+'URL_27',
+'URL_28',
+'URL_29',
+'URL_30',
+'URL_31',
+'URL_32',
+'URL_33',
+'URL_34',
+'URL_35',
+'URL_36',
+'URL_37',
+'URL_38',
+'URL_39',
+'URL_40',
+'URL_41',
+'URL_42',
+'URL_43',
+'URL_44',
+'URL_45',
+'URL_46',
+'URL_47',
+'URL_48',
+'URL_49',
+'URL_50',
+'URL_51',
+'URL_52',
+'URL_53',
+'URL_54',
+'URL_55',
+'URL_56',
+'URL_57',
+'URL_58',
+'URL_59',
+'URL_60',
+'URL_61',
+'URL_62',
+'URL_63',
+'URL_64',
+'URL_65',
+'URL_66',
+'URL_67',
+'URL_68',
+'URL_69',
+'URL_70',
+'URL_71',
+'URL_72',
+'URL_73',
+'URL_74',
+'URL_75',
+'URL_76',
+'URL_77',
+'URL_78',
+'URL_79',
+'URL_80',
+'URL_81',
+'URL_82',
+'URL_83',
+'URL_84',
+'URL_85',
+'URL_86',
+'URL_87',
+'URL_88',
+'URL_89',
+'URL_90',
+'URL_91',
+'URL_92',
+'URL_93',
+'URL_94',
+'URL_95',
+]
