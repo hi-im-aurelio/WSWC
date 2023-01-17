250d645a1b1e8b12f4c9a48c0cb6a97f7d21978b
farioso_fernando <farioso.f@gmail.com>
2023-01-17 17:33:12 +0200

    added: arquivo de binario adicionado

--- browser.py
+++ browser.py
@@ -3,9 +3,18 @@ so = platform.system()
 
 from selenium import webdriver
 
+
+def get_binary_location():
+    with open('config/binary_location.coc') as file:
+        return file.read().strip()
+
 def browser():
     if so == "Windows":
         return webdriver.Firefox(executable_path="config/geckodriver.exe")
     elif so == "Linux":
         return webdriver.Firefox()
-        
+
+
+if __name__ == "__main__":
+    # added for test compilation
+    print(get_binary_location())
