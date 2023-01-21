3394bb241cb168e834c231a160ca0b19ffbd7ee1
farioso_fernando <farioso.f@gmail.com>
2023-01-21 16:21:22 +0200

    feature: model pep adicionado.

---
+++ app/models/pep_co_mz.py
@@ -0,0 +1,67 @@
+url1 = 'https://www.pep.co.mz/categoria/roupa-para-senhora/roupa-exterior'
+url2 = 'https://www.pep.co.mz/categoria/roupa-para-senhora/acessorios'
+url3 = 'https://www.pep.co.mz/categoria/roupa-para-senhora/calcado'
+url4 = 'https://www.pep.co.mz/categoria/roupa-para-senhora/ginastica'
+url5 = 'https://www.pep.co.mz/categoria/roupa-para-senhora/higiene-e-cuidados-com-a-pele'
+url6 = 'https://www.pep.co.mz/categoria/roupa-para-homem/roupa-exterior-2'
+url7 = 'https://www.pep.co.mz/categoria/roupa-para-homem/acessorios-2'
+url8 = 'https://www.pep.co.mz/categoria/roupa-para-homem/calcado-2'
+url9 = 'https://www.pep.co.mz/categoria/roupa-para-homem/higiene-e-cuidados-com-a-pele-2'
+url10 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/bella-gema'
+url11 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/future-hero'
+url12 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/o-bebe-0-24-meses'
+url13 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/a-bebe-0-24-meses'
+url14 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/menino-2-8-anos'
+url15 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/menina-2-8-anos'
+url16 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/rapaz-8-14-anos'
+url17 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/menina-mais-crescida-8-14-anos'
+url18 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/calcado-3'
+url19 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/acessorios-para-bebe'
+url20 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/acessorios-para-menina'
+url21 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/acessorios-para-rapaz'
+url22 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/roupa-de-dormir-e-roupa-interior-para-menina'
+url23 = 'https://www.pep.co.mz/categoria/bebes-e-criancas/roupa-de-dormir-e-roupa-interior-para-rapaz'
+url24 = 'https://www.pep.co.mz/categoria/lar/cozinha'
+url25 = 'https://www.pep.co.mz/categoria/lar/quarto'
+url26 = 'https://www.pep.co.mz/categoria/lar/casa-de-banho'
+url27 = 'https://www.pep.co.mz/categoria/lar/artigos-de-limpeza'
+url30 = 'https://www.pep.co.mz/categoria/escola/calcado-4'
+url31 = 'https://www.pep.co.mz/categoria/escola/artigos-de-papelaria'
+url32 = 'https://www.pep.co.mz/categoria/pep-cell/telemoveis'
+url33 = 'https://www.pep.co.mz/categoria/pep-cell/acessorios-3'
+
+ALL_URLS = [
+    url1,
+    url2,
+    url3,
+    url4,
+    url5,
+    url6,
+    url7,
+    url8,
+    url9,
+    url10,
+    url11,
+    url12,
+    url13,
+    url14,
+    url15,
+    url16,
+    url17,
+    url18,
+    url19,
+    url20,
+    url21,
+    url22,
+    url23,
+    url24,
+    url25,
+    url26,
+    url27,
+    url30,
+    url31,
+    url32,
+    url33
+]
+
+NAMES = ['url1','url2','url3','url4','url5','url6','url7','url8','url9','url10','url11','url12','url13','url14','url15','url16','url17','url18','url19','url20','url21','url22','url23','url24','url25','url26','url27','url30','url31','url32','url33',]
